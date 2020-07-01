0b9d783bb1bebb243b9840053125df12819ef9ca
R2VM <vivekraoba@gmail.com>
2020-07-01 11:40:42 +0100

    1jul

--- src/main/java/Features/login.feature
+++ src/main/java/Features/login.feature
@@ -1,6 +1,9 @@
 Feature: Login feature of nopCommerce website
 
   Scenario: login page test Scenario
-    Given user is on login page
-    When user enters username and password and clicks login button
-    Then user enters samsung and clicks search button
+  Given user is on login page
+  When user enters username and password and clicks login button
+  Then user enters samsung and clicks search button
+    #Then user clicks on Add to Cart button/
+    #Then user navigates to Shopping cart and click on Goto cart button/
+
--- src/main/java/stepDefinitions/LoginStepDefinition.java
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -11,14 +11,14 @@ public class LoginStepDefinition {
     WebDriver driver;
     //WebDriver Manager;
 
-
-       @Given("^user is on login page$")
+        @Given("^user is on login page$")
         public void userIsOnLoginPage() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\avkin\\Downloads\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
         driver.manage().window().maximize();
-        driver.findElement(By.id("Email")).sendKeys("vivekraoba@gmail.com");
+        driver.navigate().refresh();
+        driver.findElement(By.id("Email")).sendKeys("test1@gmail.com");
         driver.findElement(By.id("Password")).sendKeys("Test@123");
         Thread.sleep(1000);
 
@@ -33,8 +33,20 @@ public class LoginStepDefinition {
 
     @Then("^user enters samsung and clicks search button$")
     public void userEntersSamsungAndClicksSearchButton() throws InterruptedException{
-           driver.findElement(By.className("ui-autocomplete-input")).sendKeys("Samsung");
+           driver.findElement(By.className("ui-autocomplete-input")).sendKeys("Apple");
            Thread.sleep(1000);
            driver.findElement(By.className("search-box-button")).click();
+           Thread.sleep(1000);
     }
+
+   // @Then("^user clicks on Add to Cart button$")
+    //public void userClicksOnAddToCartButton() {
+      //     driver.findElement(By.className("product-box-add-to-cart-button")).click();
+   // }
+
+   // @Then("^user navigates to Shopping cart and click on Goto cart button$")
+   // public void userNavigatesToShoppingCartAndClickOnGotoCartButton() {
+           //driver.findElement(By.xpath("//*[@id=\"topcartlink\"]/a/span[1]"));
+          // driver.findElement(By.xpath("//*[@id=\"add-to-cart-button-4\"]")).click();
+   // }
 }
